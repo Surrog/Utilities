583e51933514db63e22cfaabf554180f7150c05b
Surogate <errata.dark@gmail.com>
2013-11-30 16:07:18 +0100

    Merge

--- search/search/main.cpp
+++ search/search/main.cpp
@@ -13,50 +13,89 @@ int main(int argc, char **argv) {
   for (int i = 1; i < argc; i++) {
     std::string arg = argv[i];
     if (arg[0] == '-') {
-      for (char c : arg) {
-        if (c == 'r')
-          input.recursive = true;
-        if (c == 'c')
-          input.content = true;
-        if (c == 'd')
-          input.directoryName = true;
-        if (c == 't') {
-          i++;
-          if (i < argc) {
-            input.root = argv[i];
-          } else {
-            std::cout << "error : no target chosen" << std::endl;
-            result = 1;
-          }
-        }
-      }
+		if (arg[1] == 'f')
+		{
+			if (arg[2] == 'n')
+				input.filterfilename = true;
+			if (arg[2] == 'd')
+				input.filterdirectoryName = true;
+
+			i++;
+			if (i < argc)
+			{
+				input.filterEx = argv[i];
+			}
+			else
+			{
+				std::cout << "error : filter not found";
+				result = 1;
+			}
+		}
+		else
+		{
+			for (char c : arg) {
+				if (c == 'r')
+					input.recursive = true;
+				if (c == 'c')
+					input.content = true;
+				if (c == 'd')
+					input.directoryName = true;
+				if (c == 'n')
+					input.filename = true;
+				if (c == 't') {
+					i++;
+					if (i < argc) {
+						input.root = argv[i];
+					}
+					else {
+						std::cout << "error : no target chosen" << std::endl;
+						result = 2;
+					}
+				}
+			}
+		}
+		
+	  
     } else {
       regexValue = std::move(arg);
     }
   }
 
+  if (!input.filename && !input.directoryName && !input.content)
+  {
+	  input.filename = true;
+  }
+
   if (!result && !regexValue.size()) {
     std::cout << "error: no search value" << std::endl;
-    result = 2;
+    result = 3;
   }
   input.regex = regexValue;
 
   if (!result) {
-    std::cout << "search \"" << input.regex << std::endl;
+    std::cout << "search \"" << input.regex << "\"" << std::endl;
     std::cout << "target " << input.root << std::endl;
     std::cout << "recursive " << input.recursive << std::endl;
+	std::cout << "filename " << input.filename << std::endl;
     std::cout << "directory name " << input.directoryName << std::endl;
-    std::cout << "file content " << input.content << std::endl << std::endl;
-    search_task task(input);
+	std::cout << "file content " << input.content << std::endl;
+	if (input.filterfilename || input.filterdirectoryName || input.filterfilecontent)
+		std::cout << "filter \"" << input.filterEx << "\"" << std::endl;
+	std::cout << "filter on filename " << input.filterfilename << std::endl;
+	std::cout << "filter on directory name " << input.filterdirectoryName << std::endl;
+	/*std::cout << "filter on file content " << input.filterfilecontent << std::endl;*/
+	std::cout << std::endl;
+	search_task task(input);
     task.do_search();
-    for (auto error : task.getOutput().fileOpenError) {
-      std::cout << "cannot open file " << error << std::endl;
-    }
   } else {
-    std::cout << "search [-crd] [-t root path] regex\r\n-c search in file "
-                 "content\r\n-r recurse on sub directory\r\n-d search in "
-                 "directory name\t\n-t follow by a path, set the search "
-                 "location";
+	  std::cout << "search [-rcnd] [-t root_path] [-f[nd] filter_regex] regex\r\n"
+		  "-n search in filename\r\n"
+		  "-c search in file content\r\n"
+		  "-d search in directory name\t\n"
+		  "-r recurse on sub directory\r\n"
+		  "-t followed by a path, set the search location\r\n"
+		  "-f [nd] followed by a pattern: will cut short the recursion or content search";
+	std::cout.flush();
   }
   return result;
 }
--- search/search/search_task.cpp
+++ search/search/search_task.cpp
@@ -18,50 +18,61 @@ string_pool search_task::output_path(const boost::filesystem::path &input) {
   return result;
 }
 
+std::vector<pair_stringpool_int,
+	boost::pool_allocator<pair_stringpool_int> > search_task::search_content(const boost::filesystem::path &path, const std::string& regex)
+{
+	std::vector<pair_stringpool_int,
+		boost::pool_allocator<pair_stringpool_int> > result;
+	std::ifstream stream(path.string());
+	if (stream.good()) {
+		stream.sync_with_stdio(false);
+		string_pool buffer;
+		buffer.reserve(128);
+		int line = 0;
+		while (std::getline(stream, buffer)) {
+			if (find(buffer, regex))
+			{
+				int size = buffer.size();
+				result.push_back(std::make_pair(std::move(buffer), line));
+				buffer.reserve(size);
+			}
+			line++;
+		}
+	}
+	return result;
+}
+
 void search_task::search_file(const boost::filesystem::path &path) {
 	if (find(path.filename().native(), _input.regex)) // search on name
   {
     std::lock_guard<std::mutex> g(_output.coutLock);
-    std::cout << output_path(path).c_str() << std::endl;
+	std::cout << output_path(path).c_str() << "\r\n";
   }
+
+  if (_input.filterfilename && !find(path.leaf().string(), _input.filterEx))
+	  return;
 
   if (_input.content) // search in content
   {
-    std::ifstream stream(path.c_str());
-    if (stream.good()) {
-      stream.sync_with_stdio(false);
-      string_pool buffer;
-      buffer.reserve(128);
-      std::vector<pair_stringpool_int,
-                  boost::pool_allocator<pair_stringpool_int> > result_search;
-      int line = 0;
-      while (std::getline(stream, buffer)) {
-        if (find(buffer, _input.regex))
-          result_search.push_back(std::make_pair(std::move(buffer), line));
-        line++;
-      }
-      if (result_search.size()) // matching line found
-      {
-        std::lock_guard<std::mutex> g(_output.coutLock);
-        std::cout << output_path(path) << "\r\n";
-        for (auto pair : result_search) {
-          std::cout << "line " << pair.second << " : " << pair.first << "\r\n";
-        }
-        std::cout.flush();
-      }
-    } else // open file error
-    {
-      std::lock_guard<std::mutex> g(_output.coutLock);
-      _output.fileOpenError.push_back(output_path(path));
-    }
+	  auto result = search_content(path, _input.regex);
+	  if (result.size()) {
+		  std::lock_guard<std::mutex> g(_output.coutLock);
+		  std::cout << output_path(path) << "\r\n";
+		  for (auto pair : result) {
+			  std::cout << "line " << pair.second << " : " << pair.first << "\r\n";
+		  }
+	  }
   }
 }
 
 void search_task::search_directory(const boost::filesystem::path &path) {
   if (_input.directoryName && find(path.leaf().string(), _input.regex)) {
     std::lock_guard<std::mutex> g(_output.coutLock);
-    std::cout << output_path(path) << std::endl;
+	std::cout << output_path(path) << "\r\n";
   }
+
+  if (_input.filterdirectoryName && !find(path.leaf().string(), _input.filterEx))
+	  return;
 
   std::vector<std::future<void> > directory_finished;
   std::for_each(
@@ -74,6 +85,7 @@ void search_task::search_directory(const boost::filesystem::path &path) {
   for (auto &future : directory_finished) {
     future.wait();
   }
+  std::cout.flush();
 }
 
 void search_task::search(boost::filesystem::path path, bool recurcive) {
