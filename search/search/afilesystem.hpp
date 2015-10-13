ca8ac90f0629f37669e0121b9adb8dd2a5bb5a88
Ancel Francois <francoisancel@gmail.com>
2015-10-13 18:34:56 +0200

    better filesystem abstraction

--- search/search/afilesystem.hpp
+++ search/search/afilesystem.hpp
@@ -11,6 +11,8 @@ namespace filesystem
 {
    typedef std::experimental::filesystem::path path;
    typedef std::experimental::filesystem::directory_iterator directory_iterator;
+   typedef std::experimental::filesystem::file_status file_status;
+   typedef std::experimental::filesystem::file_type file_type;
    typedef std::error_code error_code;
    using std::experimental::filesystem::current_path;
    using std::experimental::filesystem::exists;
@@ -18,6 +20,7 @@ namespace filesystem
    using std::experimental::filesystem::is_regular_file;
    using std::experimental::filesystem::is_directory;
    using std::experimental::filesystem::status;
+   using std::experimental::filesystem::last_write_time;
 }
 }
 
@@ -33,6 +36,73 @@ namespace filesystem
 {
    typedef boost::filesystem::path path;
    typedef boost::filesystem::directory_iterator directory_iterator;
+   typedef boost::filesystem::file_status file_status;
+   struct file_type
+   {
+      enum Type {
+         not_found = boost::filesystem::file_not_found,
+         none = boost::filesystem::status_unknown,
+         regular = boost::filesystem::regular_file,
+         directory = boost::filesystem::directory_file,
+         symlink = boost::filesystem::symlink_file,
+         block = boost::filesystem::block_file,
+         character = boost::filesystem::character_file,
+         fifo = boost::filesystem::fifo_file,
+         socket = boost::filesystem::socket_file,
+         unknown = boost::filesystem::type_unknown
+      };
+   private:
+      Type _value;
+   public:
+
+      file_type(Type value)
+         : _value(value)
+      {}
+
+      file_type(boost::filesystem::file_type value)
+         : _value(static_cast<Type>(value))
+      {}
+
+      operator Type()
+      {
+         return _value;
+      }
+      operator boost::filesystem::file_type()
+      {
+         return static_cast<boost::filesystem::file_type>(_value);
+      }
+
+      Type get() const { return _value; }
+
+      bool operator<(const file_type& rvalue) const
+      {
+         return _value < rvalue._value;
+      }
+
+      template <typename T>
+      bool operator<(const T&& rvalue) const
+      {
+         return _value < rvalue._value;
+      }
+
+      template <typename T>
+      bool operator>(const T&& rvalue) const
+      {
+         return !operator<(rvalue);
+      }
+
+      bool operator==(const file_type& rvalue) const
+      {
+         return _value == rvalue._value;
+      }
+
+      template <typename T>
+      bool operator==(const T&& rvalue) const
+      {
+         return _value == rvalue._value;
+      }
+   };
+   
    typedef boost::system::error_code error_code;
    using boost::filesystem::current_path;
    using boost::filesystem::exists;
@@ -40,6 +110,7 @@ namespace filesystem
    using boost::filesystem::is_regular_file;
    using boost::filesystem::is_directory;
    using boost::filesystem::status;
+   using boost::filesystem::last_write_time;
 }
 }
 
