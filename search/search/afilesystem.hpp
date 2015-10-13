3c11857c8221da529d39c99c950d24b089f1f8ad
Ancel Francois <francoisancel@gmail.com>
2015-10-13 16:52:58 +0200

    update filesystem

--- search/search/afilesystem.hpp
+++ search/search/afilesystem.hpp
@@ -3,6 +3,7 @@
 
 #if _MSC_VER > 1700
 #include <experimental/filesystem>
+#include <system_error>
 
 namespace astd
 {
@@ -10,10 +11,13 @@ namespace filesystem
 {
    typedef std::experimental::filesystem::path path;
    typedef std::experimental::filesystem::directory_iterator directory_iterator;
+   typedef std::error_code error_code;
+   using std::experimental::filesystem::current_path;
    using std::experimental::filesystem::exists;
    using std::experimental::filesystem::remove;
    using std::experimental::filesystem::is_regular_file;
    using std::experimental::filesystem::is_directory;
+   using std::experimental::filesystem::status;
 }
 }
 
@@ -21,6 +25,7 @@ namespace filesystem
 
 #include "boost/filesystem/path.hpp"
 #include "boost/filesystem/operations.hpp"
+#include "boost/system/error_code.hpp"
 
 namespace astd
 {
@@ -28,10 +33,13 @@ namespace filesystem
 {
    typedef boost::filesystem::path path;
    typedef boost::filesystem::directory_iterator directory_iterator;
+   typedef boost::system::error_code error_code;
+   using boost::filesystem::current_path;
    using boost::filesystem::exists;
    using boost::filesystem::remove;
    using boost::filesystem::is_regular_file;
    using boost::filesystem::is_directory;
+   using boost::filesystem::status;
 }
 }
 
